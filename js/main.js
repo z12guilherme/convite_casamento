db5bc6a9719ae02370b9e4542436a0ebb8a01bdf
Marcos Guilherme <marcos@gmail.com>
2025-10-21 15:17:13 -0300

    Colocando convidados no BD

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // ================== Supabase ==================
 const supabaseUrl = 'https://ccaycdgjpmffkkrpppwv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjYXljZGdqcG1mZmtrcnBwcHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNDUyNTMsImV4cCI6MjA3NDgyMTI1M30.G76CIatcTs3OxwB6VyWKcbDHhE4kDBGQ0OVavQ52WhM';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 // ================== Convidados ==================
 async function adicionarConvidado() {
