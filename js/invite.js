fc608867ffdcd3bbcbdea975ef80c4846d4a7895
unknown <mguimarcos39@gmail.com>
2025-11-04 15:09:52 -0300

    Ajustes

--- js/invite.js
+++ js/invite.js
@@ -2,6 +2,7 @@
 
   const supabaseUrl = 'https://ccaycdgjpmffkkrpppwv.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjYXljZGdqcG1mZmtrcnBwcHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNDUyNTMsImV4cCI6MjA3NDgyMTI1M30.G76CIatcTs3OxwB6VyWKcbDHhE4kDBGQ0OVavQ52WhM';
+  console.log('window.supabase before createClient:', window.supabase);
   const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
   function sanitizeHTML(str) {
