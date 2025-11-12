655aed7c4c0b8491c4a05d5d91d94ac6ec529dad
unknown <mguimarcos39@gmail.com>
2025-11-12 14:18:42 -0300

    Corrigindo Bug no painel de Presentes

--- js/gift-dashboard.js
+++ js/gift-dashboard.js
@@ -1,7 +1,3 @@
-const supabaseUrl = 'https://ccaycdgjpmffkkrpppwv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjYXljZGdqcG1mZmtrcnBwcHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNDUyNTMsImV4cCI6MjA3NDgyMTI1M30.G76CIatcTs3OxwB6VyWKcbDHhE4kDBGQ0OVavQ52WhM';
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
 const availableBody = document.getElementById('available-gifts');
 const confirmedBody = document.getElementById('confirmed-gifts');
 const addGiftForm = document.getElementById('add-gift-form');
